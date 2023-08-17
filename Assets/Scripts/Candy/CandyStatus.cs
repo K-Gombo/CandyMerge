2a827b0b32c6789436064d443d08729b0913a545
KGB <125139072+CaptainGombo@users.noreply.github.com>
2023-08-17 11:19:48 +0900

    Feat.캔디레벨 표시 버튼 구현

--- Assets/Scripts/Candy/CandyStatus.cs
+++ Assets/Scripts/Candy/CandyStatus.cs
@@ -1,11 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CandyStatus : MonoBehaviour
 {
     public int level; // 캔디의 레벨을 저장하는 변수
+    public Text levelText; // 레벨을 표시할 텍스트 컴포넌트
+    public GameObject levelTextObject; // 레벨 텍스트 오브젝트
+
+    private void Start()
+    {
+        ToggleLevelText(false); // 레벨 텍스트 오브젝트를 비활성화
+        UpdateLevelText();
+    }
+
+    private void Update()
+    {
+        levelText.text = level.ToString(); 
+    }
 
     private void OnDestroy()
     {
-        CandyManager.instance.CandyDestroyed(); // 캔디가 파괴될 때마다 CandyManager에 알림
+        CandyManager.instance.CandyDestroyed();
+    }
+
+    public void ToggleLevelText(bool show)
+    {
+        levelTextObject.SetActive(show); // 레벨 텍스트 오브젝트의 활성화 상태 설정
+    }
+
+    private void UpdateLevelText()
+    {
+        levelText.text = level.ToString(); 
     }
 }
--- Assets/Scripts/PlayScripts/LevelBtn.cs
+++ Assets/Scripts/PlayScripts/LevelBtn.cs
@@ -1,10 +1,9 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI; // UI 컴포넌트를 사용하기 위해 추가
+using UnityEngine.UI;
 
 public class LevelBtn : MonoBehaviour
 {
-    public Button LevelOffBtn; // Button 타입으로 변경
+    public Button LevelOffBtn;
     public Button LevelOnBtn;
 
     private void Start()
@@ -12,26 +11,32 @@ public class LevelBtn : MonoBehaviour
         LevelOffBtn.gameObject.SetActive(true);
         LevelOnBtn.gameObject.SetActive(false);
 
-        // 버튼에 클릭 리스너 추가
-        LevelOffBtn.onClick.AddListener(OnAMOffBtnClick);
-        LevelOnBtn.onClick.AddListener(OnAMOnBtnClick);
+        LevelOffBtn.onClick.AddListener(OnLevelOffBtnClick);
+        LevelOnBtn.onClick.AddListener(OnLevelOnBtnClick);
     }
 
-    // ACOffBtn 클릭 시 호출될 메서드
-    private void OnAMOffBtnClick()
+    private void OnLevelOffBtnClick()
     {
         Debug.Log("LevelOffBtn 클릭됨!");
         LevelOffBtn.gameObject.SetActive(false);
         LevelOnBtn.gameObject.SetActive(true);
+        ToggleLevelText(true);
     }
 
-    // ACOnBtn 클릭 시 호출될 메서드
-    private void OnAMOnBtnClick()
+    private void OnLevelOnBtnClick()
     {
-        
         Debug.Log("LevelOnBtn 클릭됨!");
         LevelOffBtn.gameObject.SetActive(true);
         LevelOnBtn.gameObject.SetActive(false);
-        
+        ToggleLevelText(false);
+    }
+
+    private void ToggleLevelText(bool show)
+    {
+        CandyStatus[] candies = FindObjectsOfType<CandyStatus>();
+        foreach (CandyStatus candy in candies)
+        {
+            candy.ToggleLevelText(show);
+        }
     }
 }
