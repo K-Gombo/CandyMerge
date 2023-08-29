cf31209bfe66639796725c7a6b637aa313bc925d
KGB <125139072+CaptainGombo@users.noreply.github.com>
2023-08-29 15:36:37 +0900

    g

--- Assets/Scripts/Equip/OvenPanelBtn.cs
+++ Assets/Scripts/Equip/OvenPanelBtn.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class UpgradePanelBtn : MonoBehaviour
+public class OvenPanelBtn : MonoBehaviour
 {
     public List<GameObject> allPanels; // 모든 패널을 관리하는 리스트
-    public GameObject UpgradePanel;    // UpgradePanel
+    public GameObject OvenPanel;    // UpgradePanel
 
     public void OnButtonClick()
     {
@@ -14,7 +14,7 @@ public class UpgradePanelBtn : MonoBehaviour
             panel.SetActive(false);
         }
 
-        // UpgradePanel 활성화
-        UpgradePanel.SetActive(true);
+    
+        OvenPanel.SetActive(true);
     }
 }
