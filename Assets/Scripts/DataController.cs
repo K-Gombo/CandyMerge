8e42c42cdf080cb889a022f8b63093dffeaf50f6
SpartaJungWoo <114643828+SpartaJungWoo@users.noreply.github.com>
2023-09-13 21:55:03 +0900

    Upgrade 저장 기초공사

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -6,7 +6,7 @@ public class DataController : MonoBehaviour
 {
     public static DataController instance;
     [SerializeField] GiftBoxController giftBoxController;
-
+    [SerializeField] UpgradeManager upgradeManager;
 
     private void Awake()
     {
@@ -19,6 +19,15 @@ public class DataController : MonoBehaviour
         CandiesRemaining_Load();
         Player_Level_Load();
         Player_Experience_Load();
+        Upgrade_luckyCreateLevel_Load();
+        Upgrade_createSpeedLevel_Load();
+        Upgrade_removeLockedLevel_Load();
+        Upgrade_maxCandiesLevel_Load();
+        Upgrade_candyLevel_Load();
+        Upgrade_passiveAutoCreateSpeedLevel_Load();
+        Upgrade_goldUpLevel_Load();
+        Upgrade_luckyGoldLevel_Load();
+
 
         HappyLevel.instance.InitUI();
     }
@@ -83,5 +92,174 @@ public class DataController : MonoBehaviour
     {
         Player_Experience_DataLoad();
         HappyLevel.instance.currentExperience = GameData.player_Experience;
+    }
+
+
+    // Upgrade
+    private void Upgrade_luckyCreateLevel_DataSave()
+    {
+        ES3.Save("upgrade_luckyCreateLevel", GameData.luckyCreateLevel);
+    }
+    public void Upgrade_luckyCreateLevel_Save()
+    {
+        GameData.luckyCreateLevel = upgradeManager.luckyCreateLevel;
+        Upgrade_luckyCreateLevel_DataSave();
+    }
+
+    private void Upgrade_luckyCreateLevel_DataLoad()
+    {
+        GameData.luckyCreateLevel = ES3.Load("upgrade_luckyCreateLevel", 1);
+    }
+    public void Upgrade_luckyCreateLevel_Load()
+    {
+        Upgrade_luckyCreateLevel_DataLoad();
+        upgradeManager.luckyCreateLevel = GameData.luckyCreateLevel;
+    }
+
+
+    private void Upgrade_createSpeedLevel_DataSave()
+    {
+        ES3.Save("upgrade_createSpeedLevel", GameData.createSpeedLevel);
+    }
+    public void Upgrade_createSpeedLevel_Save()
+    {
+        GameData.createSpeedLevel = upgradeManager.createSpeedLevel;
+        Upgrade_createSpeedLevel_DataSave();
+    }
+
+    private void Upgrade_createSpeedLevel_DataLoad()
+    {
+        GameData.createSpeedLevel = ES3.Load("upgrade_createSpeedLevel", 1);
+    }
+    public void Upgrade_createSpeedLevel_Load()
+    {
+        Upgrade_createSpeedLevel_DataLoad();
+        upgradeManager.createSpeedLevel = GameData.createSpeedLevel;
+    }
+
+
+    private void Upgrade_removeLockedLevel_DataSave()
+    {
+        ES3.Save("upgrade_removeLockedLevel", GameData.removeLockedLevel);
+    }
+    public void Upgrade_removeLockedLevel_Save()
+    {
+        GameData.removeLockedLevel = upgradeManager.removeLockedLevel;
+        Upgrade_removeLockedLevel_DataSave();
+    }
+
+    private void Upgrade_removeLockedLevel_DataLoad()
+    {
+        GameData.removeLockedLevel = ES3.Load("upgrade_removeLockedLevel", 1);
+    }
+    public void Upgrade_removeLockedLevel_Load()
+    {
+        Upgrade_removeLockedLevel_DataLoad();
+        upgradeManager.removeLockedLevel = GameData.removeLockedLevel;
+    }
+
+
+    private void Upgrade_maxCandiesLevel_DataSave()
+    {
+        ES3.Save("upgrade_maxCandiesLevel", GameData.maxCandiesLevel);
+    }
+    public void Upgrade_maxCandiesLevel_Save()
+    {
+        GameData.maxCandiesLevel = upgradeManager.maxCandiesLevel;
+        Upgrade_maxCandiesLevel_DataSave();
+    }
+
+    private void Upgrade_maxCandiesLevel_DataLoad()
+    {
+        GameData.maxCandiesLevel = ES3.Load("upgrade_maxCandiesLevel", 1);
+    }
+    public void Upgrade_maxCandiesLevel_Load()
+    {
+        Upgrade_maxCandiesLevel_DataLoad();
+        upgradeManager.maxCandiesLevel = GameData.maxCandiesLevel;
+    }
+
+
+    private void Upgrade_candyLevel_DataSave()
+    {
+        ES3.Save("upgrade_candyLevel", GameData.candyLevel);
+    }
+    public void Upgrade_candyLevel_Save()
+    {
+        GameData.candyLevel = upgradeManager.candyLevel;
+        Upgrade_candyLevel_DataSave();
+    }
+
+    private void Upgrade_candyLevel_DataLoad()
+    {
+        GameData.candyLevel = ES3.Load("upgrade_candyLevel", 1);
+    }
+    public void Upgrade_candyLevel_Load()
+    {
+        Upgrade_candyLevel_DataLoad();
+        upgradeManager.candyLevel = GameData.candyLevel;
+    }
+
+
+    private void Upgrade_passiveAutoCreateSpeedLevel_DataSave()
+    {
+        ES3.Save("upgrade_passiveAutoCreateSpeedLevel", GameData.passiveAutoCreateSpeedLevel);
+    }
+    public void Upgrade_passiveAutoCreateSpeedLevel_Save()
+    {
+        GameData.passiveAutoCreateSpeedLevel = upgradeManager.passiveAutoCreateSpeedLevel;
+        Upgrade_passiveAutoCreateSpeedLevel_DataSave();
+    }
+
+    private void Upgrade_passiveAutoCreateSpeedLevel_DataLoad()
+    {
+        GameData.passiveAutoCreateSpeedLevel = ES3.Load("upgrade_passiveAutoCreateSpeedLevel", 1);
+    }
+    public void Upgrade_passiveAutoCreateSpeedLevel_Load()
+    {
+        Upgrade_passiveAutoCreateSpeedLevel_DataLoad();
+        upgradeManager.passiveAutoCreateSpeedLevel = GameData.passiveAutoCreateSpeedLevel;
+    }
+
+
+    private void Upgrade_goldUpLevel_DataSave()
+    {
+        ES3.Save("upgrade_goldUpLevel", GameData.goldUpLevel);
+    }
+    public void Upgrade_goldUpLevel_Save()
+    {
+        GameData.goldUpLevel = upgradeManager.goldUpLevel;
+        Upgrade_goldUpLevel_DataSave();
+    }
+
+    private void Upgrade_goldUpLevel_DataLoad()
+    {
+        GameData.goldUpLevel = ES3.Load("upgrade_goldUpLevel", 1);
+    }
+    public void Upgrade_goldUpLevel_Load()
+    {
+        Upgrade_goldUpLevel_DataLoad();
+        upgradeManager.goldUpLevel = GameData.goldUpLevel;
+    }
+
+
+    private void Upgrade_luckyGoldLevel_DataSave()
+    {
+        ES3.Save("upgrade_luckyGoldLevel", GameData.luckyGoldLevel);
+    }
+    public void Upgrade_luckyGoldLevel_Save()
+    {
+        GameData.luckyGoldLevel = upgradeManager.luckyGoldLevel;
+        Upgrade_luckyGoldLevel_DataSave();
+    }
+
+    private void Upgrade_luckyGoldLevel_DataLoad()
+    {
+        GameData.luckyGoldLevel = ES3.Load("upgrade_luckyGoldLevel", 1);
+    }
+    public void Upgrade_luckyGoldLevel_Load()
+    {
+        Upgrade_luckyGoldLevel_DataLoad();
+        upgradeManager.luckyGoldLevel = GameData.luckyGoldLevel;
     }
 }
--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -8,4 +8,15 @@ public static class GameData
     public static int player_Experience = 0;
 
     public static int candiesRemaining = 0;
+
+
+    // Upgrade
+    public static int luckyCreateLevel = 1;
+    public static int createSpeedLevel = 1;
+    public static int removeLockedLevel = 1;
+    public static int maxCandiesLevel = 1;
+    public static int candyLevel = 1;
+    public static int passiveAutoCreateSpeedLevel = 1;
+    public static int goldUpLevel = 1;
+    public static int luckyGoldLevel = 1;
 }
