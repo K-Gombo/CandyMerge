89e7b9f5bb509a30d6fef36709f13b1e453a4c27
SpartaJungWoo <114643828+SpartaJungWoo@users.noreply.github.com>
2023-09-22 00:03:14 +0900

    앱 종료시에도 쿨타임 돌게

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -10,50 +10,39 @@ public class Timer : MonoBehaviour
 
     [SerializeField] Button adsButton;
 
+
     void Update()
     {
         CheckCooldown();
     }
 
-    // 이 메서드는 광고가 성공적으로 보여진 후에 호출됩니다.
     public void OnAdWatched()
     {
         // 현재 시간을 저장
         DateTime now = DateTime.UtcNow;
-        PlayerPrefs.SetString(LastAdTimeKey, now.ToBinary().ToString());
-        PlayerPrefs.Save();
+        ES3.Save<DateTime>(LastAdTimeKey, now);
     }
 
-    // 앱이 활성화되거나, 쿨타임을 체크하려는 경우에 호출됩니다.
     public void CheckCooldown()
     {
-        // 마지막 광고 시간을 불러옴
-        string lastAdTimeStr = PlayerPrefs.GetString(LastAdTimeKey, string.Empty);
-
-        if (string.IsNullOrEmpty(lastAdTimeStr))
+        if (!ES3.KeyExists(LastAdTimeKey))
         {
-            // 쿨타임 정보가 없으면 광고를 볼 수 있음
             EnableAdButton();
             return;
         }
 
-        DateTime lastAdTime = DateTime.FromBinary(Convert.ToInt64(lastAdTimeStr));
+        Debug.Log("시간이 다!");
+        DateTime lastAdTime = ES3.Load<DateTime>(LastAdTimeKey);
         DateTime now = DateTime.UtcNow;
-
-        // 시간 차이 계산
         TimeSpan diff = now - lastAdTime;
 
         if (diff.TotalMinutes >= CooldownMinutes)
         {
-            // 쿨타임이 지났으므로 광고를 볼 수 있음
             EnableAdButton();
         }
         else
         {
-            // 쿨타임이 아직 지나지 않았으므로 광고를 볼 수 없음
             DisableAdButton();
-
-            // 남은 시간을 Text UI로 표시
             TimeSpan remainingTime = TimeSpan.FromMinutes(CooldownMinutes) - diff;
             cooldownText.text = string.Format("{0:D2}:{1:D2}", remainingTime.Minutes, remainingTime.Seconds);
         }
